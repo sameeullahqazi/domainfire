60b121844ce47b491cd215863bfef5eb16d6abdd
sameeullahqazi <samee@lamproslabs.com>
2018-03-12 11:45:18 +0500

    layout

---
+++ layouts/default.layout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="/js/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	
+});
+</script>
+</head>
+<body>
+<?php echo $content_for_layout; ?>
+</body>
+</html>
